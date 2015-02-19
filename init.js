1a92545b3bc14b57c6c232fad19b5d53ace5d150
Methacton Electric Car Club <methactonecc@gmail.com>
2015-02-19 15:48:27 -0500

    Update init.js

--- init.js
+++ init.js
@@ -217,7 +217,7 @@ function handleMouseDown(event)
     var distX = Math.abs(shotX - spriteX);
     var distY = Math.abs(shotY - spriteY);
 	
-	console.table(animation);
+	console.log(animation);
     // Anywhere in the body or head is a hit - but not the wings
     if(distX < 30 && distY < 59 && animation.spriteSheet === spriteSheet)
     {
